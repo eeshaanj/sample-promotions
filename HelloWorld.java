4064bfccf4438d8f35b267735e795e7c9cf5a06b
Eeshaan Joshi <eeshaan_joshi@hotmail.com>
2019-06-04 01:55:18 +0000

    new comment line

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,9 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 	
+        System.out.println("Hello, World");
+
+    //	Prints "Welcome to Git" 	
 	System.out.println("Welcome to Git");
     } 
 } 
